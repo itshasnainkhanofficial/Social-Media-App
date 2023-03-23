4f6d51af0d717f68d2f8f86b8cea3f6c338e6200
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-03-23 18:12:22 +0500

    Friends ad remove logic added but not tested

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -35,7 +35,7 @@ export const getUserFriends = async (req, res) => {
 
             const friends = await Promise.all(
             user.friends.map((id) => id)
-            // user.friends.map((id) => User.findById(id))
+            // user.friends.map((id) => User.findById(id)) // not working
         );
 
         
@@ -46,3 +46,33 @@ export const getUserFriends = async (req, res) => {
     }
 }
 
+
+// addRemoveFriend 
+
+export const addRemoveFriend = async (req, res) => {
+    try {
+        const {id, friendId} = req.params
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);
+          } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+          }
+
+          await user.save();
+          await friend.save();
+
+          const friends = await Promise.all(
+              user.friends.map((id) => User.findById(id))
+              );
+              
+          res.status(200).json(friends);
+
+    } catch (error) {
+        res.status(404).json({ message: err.message });
+    }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   getAllUsers,
   getUser,
-  getUserFriends
+  getUserFriends,
+  addRemoveFriend
 } from "../controllers/users.js";
 
 import { verifyToken } from "../middleware/auth.js";
@@ -20,4 +21,7 @@ router.get("/:id", verifyToken , getUser )
 // Get User Friends
 router.get("/:id/friends", verifyToken , getUserFriends )
 
+
+// Update
+router.patch("/:id/:friendId", verifyToken , addRemoveFriend)
 export default router;
