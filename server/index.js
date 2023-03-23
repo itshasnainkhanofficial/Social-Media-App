9f12bd7a7d4bf4dbfda7b9062127225ddeb756b6
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-03-23 12:47:22 +0500

    Backend Authentication completed

--- server/index.js
+++ server/index.js
@@ -8,23 +8,39 @@ import helmet from "helmet"; // helps you secure your Node.js application by set
 import morgan from "morgan"; // morgan is a Node.js and Express middleware to log HTTP requests and errors, and simplifies the process
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js"
+import authRoutes from "./routes/auth.js";
+
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
-
 dotenv.config()
-
 const app = express()
+
+
+
+
+
+
+
+
+
+
+
+// Middleware
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}))
-app.use(morgan("common"))
+app.use(morgan("tiny"))
 app.use(cors())
 app.use("/assets", express.static(path.join(dirname, "public/assets"))) // storing image on localstorage, in real app it should be stored on clould like s3
-
 // disabled for checking purpose
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+
+
+
 
 // storing file on disk, the following code is from github readme https://github.com/expressjs/multer#readme
 const storage = multer.diskStorage({
@@ -37,3 +53,36 @@ const storage = multer.diskStorage({
   })
 
 const upload = multer({ storage })
+
+
+
+
+
+
+
+// Routes
+app.post("/auth/register",  upload.single("picture"), register)
+app.use("/auth", authRoutes)
+
+
+
+
+
+
+
+
+
+
+//Connecting Mongodb
+const PORT = process.env.PORT || 3001 || 6001;
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then( () => {
+
+  app.listen(PORT, () => console.log(`Server Connect on Port: ${PORT}`));
+
+}).catch((error) => console.log(`Mongodb Did not connect here is the message: ${error}`))
+
+
