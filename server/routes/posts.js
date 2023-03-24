a549519565885e78728eee4cf4a8922c24ed23f7
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-03-24 15:08:36 +0500

    getting all posts of a specific user

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { userFeedPosts } from "../controllers/posts.js";
+import { userFeedPosts, getUserPosts } from "../controllers/posts.js";
 const router = express.Router();
 
 
 // Get All Posts
 router.get("/get" , verifyToken , userFeedPosts)
+
+// get User Post
+router.get("/:userId" , verifyToken , getUserPosts)
 
 export default router;
